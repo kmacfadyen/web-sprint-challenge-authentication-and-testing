0f7572016866b5640a3057c8119864d0b49c0a38
Kevin MacFadyen <kmacfadyen3@gmail.com>
2023-06-11 22:41:22 -0400

    finishing tests

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,19 @@
 // Write your tests here
+const request = require('supertest')
+const server = require('./server')
+const db = require('../data/dbConfig')
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+afterAll(async () => {
+  await db.destroy()
+})
+
+const exampleUser = { username: 'mike', password: 'smith' }
+
 test('sanity', () => {
   expect(true).toBe(true)
 })
